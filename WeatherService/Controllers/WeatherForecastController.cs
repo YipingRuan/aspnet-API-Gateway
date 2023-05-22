8d1d6f9c4824a5db0b1662fd5c98bbfa8d6d5f54
Ruan Yiping <yiping.ruan@outlook.com>
2023-05-22 11:21:52 +0800

    Add WeatherService

---
+++ WeatherService/Controllers/WeatherForecastController.cs
@@ -0,0 +1,39 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace WeatherService.Controllers
+{
+    [ApiController]
+    [Route("[controller]/[Action]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Forecast()
+        {
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Forecast2()
+        {
+            throw new Exception("AAAA");
+        }
+    }
+}
