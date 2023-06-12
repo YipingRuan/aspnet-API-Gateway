fc46e1e06caa92eacdb80d0f234d805421b5fc42
Ruan Yiping <yiping.ruan@outlook.com>
2023-06-12 22:17:11 +0800

    Add example

---
+++ Common/ErrorHandling/Examples.cs
@@ -0,0 +1,42 @@
+﻿namespace Common.ErrorHandling
+{
+    class Examples
+    {
+        // Recover from an error
+        void Usage1()
+        {
+            try
+            {
+                File.ReadAllLines("File not there");
+            }
+            catch (Exception ex)
+            {
+                // Read from back file instead
+            }
+
+            // Normal flow continue
+        }
+
+        // Convert to a user/developer friendly coded error, only for the purpose
+        // 1. Preserve details
+        // 2. Can be translated later
+        void Usage2()
+        {
+            try
+            {
+                // ServiceA.SaveShipment(xxx)
+            }
+            catch (Exception ex)
+            {
+                // Wrap the exception with code and detail
+                throw ex.Bag("ProcessorService.SaveShipment", new { Reference = 123 });
+            }
+        }
+
+        // Otherwise just let it fail, do not use try-catch block to swallow exceptions!
+        void Usage3()
+        { 
+            // Database IO operation that may 
+        }
+    }
+}
